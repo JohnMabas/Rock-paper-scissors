3d38feeab2d21ab213c1a3b023b987b4cb939da9
JohnMabas <johnmabas1@gmail.com>
2024-08-23 10:30:59 +0100

    upgraded the functionalities

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
 
+function playerGame(playerMove) {
+    const computerMove = pickComputerMove();
+
+
+    let result = '';
+
+        if (playerMove === 'scissors') { 
+
+            if (computerMove === 'rock') {
+                result = 'you lose';
+            } else if (computerMove == 'paper') {
+                result = 'You win';
+            } else if (computerMove == 'scissors') {
+                result = 'Tie.';
+            }
+    
+        } else if (playerMove === 'paper') {
+    
+            if (computerMove === 'rock') {
+                result = 'You won.';
+            } else if (computerMove == 'paper') {
+                result = 'Tie';
+            } else if (computerMove == 'scissors') {
+                result = 'You lose.';
+            }
+    
+        } else if (playerMove === "rock") {
+            if (computerMove === 'rock') {
+                result = 'Tie';
+            } else if (computerMove == 'paper') {
+                result = 'You lose';
+            } else if (computerMove == 'scissors') {
+                result = 'You win.';
+            }
+        }
+        
+        alert(`You picked ${playerMove}. computer picked ${computerMove}. ${result}`);
+}
+
+
+
 function pickComputerMove() {
 
         const randonNumber = Math.random();
